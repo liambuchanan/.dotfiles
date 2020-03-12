90f3e053603ecf2bb147050802672d53d7f26900
Liam Buchanan <liam.buchanan@gmail.com>
2020-03-12 13:56:59 -0700

    add homebrew installation to setup script

--- setup.sh
+++ setup.sh
@@ -2,16 +2,12 @@
 
 cd $(dirname $0)
 is_ok() {
-    read -p "${1:?} [y/N]?" _is_ok
-    if [[ "$_is_ok" == "y" ]]
-    then
-        true
-    else
-        false
-    fi
+    read -p "${1:?} [y/N]? " _is_ok
+    [[ "$_is_ok" == "y" ]]
 }
-is_ok "Replace .bashrc" && rm -f ~/.bashrc && ln -s $PWD/.bashrc ~/.bashrc
-is_ok "Replace .bash_profile" && rm -f ~/.bash_profile && ln -s $PWD/.bashrc ~/.bash_profile
-is_ok "Replace .vimrc" && rm -f ~/.vimrc && ln -s $PWD/.vimrc ~/.vimrc
-is_ok "Replace .vim/autoload" && rm -f ~/.vim/autoload && ln -s $PWD/.vim/autoload ~/.vim/autoload
-is_ok "Replace .vim/bundle" && rm -f ~/.vim/bundle && ln -s $PWD/.vim/bundle ~/.vim/bundle
+[[ "$(readlink ~/.bashrc)" == "$PWD/.bashrc" ]] || (is_ok "Replace bashrc" && ln -s $PWD/.bashrc ~/.bashrc)
+[[ "$(readlink ~/.bash_profile)" == "$PWD/.bashrc" ]] || (is_ok "Replace .bash_profile" && rm -f ~/.bash_profile && ln -s $PWD/.bashrc ~/.bash_profile)
+[[ "$(readlink ~/.vimrc)" == "$PWD/.vimrc" ]] || (is_ok "Replace .vimrc" && rm -f ~/.vimrc && ln -s $PWD/.vimrc ~/.vimrc)
+[[ "$(readlink ~/.vim/autoload)" == "$PWD/.vim/autoload" ]] || (is_ok "Replace .vim/autoload" && rm -f ~/.vim/autoload && ln -s $PWD/.vim/autoload ~/.vim/autoload)
+[[ "$(readlink ~/.vim/bundle)" == "$PWD/.vim/bundle" ]] || (is_ok "Replace .vim/bundle" && rm -f ~/.vim/bundle && ln -s $PWD/.vim/bundle ~/.vim/bundle)
+[[ "$(command -v brew)" != "" ]] || (is_ok "Install homebrew" && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)")
